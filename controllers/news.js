1e5fbfcf12d545e0976773b1b100cf14d5bbfe5f
叶文俊 <13588781920@163.com>
2019-01-19 12:37:25 +0800

    fix newDetailPage  and pagination bug (#11)
    * fix newDetailPage  and pagination bug
    
    * Query optimization

--- controllers/news.js
+++ controllers/news.js
@@ -1,28 +1,42 @@
 'use strict';
-const { getNews, getNewsByChannelName } = require('../models/news');
+const {
+  getNews,
+  getNewsByChannelName,
+  getNewsTotal,
+  getChannelNewsTotal
+} = require('../models/news');
 const { checkToken } = require('./user');
 const news = () => {};
 
 const showAllNews = async (ctx, next) => {
-  // const query = ctx.query;
-  // console.log(query);
+  const channelName = ctx.query.channel;
+  const pageNumber = ctx.query.pageNumber; // 请求的新闻页号
+  console.log(channelName, pageNumber); // 查询的新闻频道
   try {
-    const newsList = await getNews();
+    let newsList = [];
+    let total = 0;
+    if (channelName === 'recommend') {
+      newsList = await getNews(pageNumber);
+      total = await getNewsTotal();
+    } else {
+      newsList = await getNewsByChannelName(channelName, pageNumber);
+      total = await getChannelNewsTotal(channelName);
+    }
     ctx.compress = true;
     const token = ctx.request.header.authorization;
-    const username = ctx.session.username;
     if (token && !checkToken(token)) {
       // 当请求头部有token时，如果token过期并且用户处于登陆状态就让用户重新登陆
       ctx.body = {
         code: 0,
-        newsList: newsList.slice(0, 20),
-        message: 'invalid token'
+        newsList: newsList,
+        message: 'invalid token',
+        total: total / 20
       };
     } else {
       ctx.body = {
         code: 0,
-        newsList: newsList.slice(0, 20),
-        username: username
+        newsList: newsList,
+        total: total / 20
       };
     }
   } catch (e) {
--- models/news.js
+++ models/news.js
@@ -2,8 +2,30 @@
 
 const { News } = require('../lib/mongo');
 
+function getNewsTotal() {
+  return News.find({}).countDocuments();
+}
+
+function getNews(pageNumber) {
+  return News.find({})
+    .skip((pageNumber - 1) * 20)
+    .limit(20);
+}
+
+function getChannelNewsTotal(channelName) {
+  return News.find({ channelName }).countDocuments();
+}
+
+function getNewsByChannelName(channelName, pageNumber) {
+  return News.find({ channelName })
+    .skip((pageNumber - 1) * 20)
+    .limit(20);
+}
+
 module.exports = {
-  getNews: () => News.find({}),
-  getNewsByChannelName: channelName => News.find({ channelName }),
+  getNews,
+  getNewsByChannelName,
+  getNewsTotal,
+  getChannelNewsTotal,
   createNews: News => new News(News).save()
 };
